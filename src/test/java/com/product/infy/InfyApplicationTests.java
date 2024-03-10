043d4156ed291eccdf8e498d7a222f42a854940d
KongaRagini <138753449+KongaRagini@users.noreply.github.com>
2024-03-10 21:20:00 +0530

    ecom project
    First commit

---
+++ src/test/java/com/product/infy/InfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.infy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
